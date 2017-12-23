d0da21db96b5dd09761118b0a52f113d09b6ac6c
minimallinux <anthonymcgowan1@gmail.com>
2017-12-23 23:29:17 +0000

    Add Footer

--- footer.php
+++ footer.php
@@ -12,7 +12,25 @@
 		</div>
 	</div>
 </div><!-- #content -->
+<footer class="footer-basic-centered">
 
+			<h4 class="footer-company-logo" >Red Diamond</h4>
+
+			<div class="footer-links">
+				<a href="#">Shop</a>
+				
+				<a href="#">About</a>
+				
+		        <a href="#">Map</a>
+				
+				<a href="#">Checkout</a>
+				
+				<a href="#">Account</a>
+			</div>
+
+			<div class="footer-company-name">Company Name &copy; 2015</div>
+
+		</footer>
 		
 <?php wp_footer(); ?>
 	
