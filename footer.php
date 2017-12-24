71b94eb2072e76a2ce1896acc5d3f558588315c5
minimallinux <anthonymcgowan1@gmail.com>
2017-12-24 14:36:40 +0000

    Update Footer

--- footer.php
+++ footer.php
@@ -33,9 +33,9 @@
 				<div class="footer three">
 					<h3>Contact Us</h3>
 					<ul>
-						<li>Red Diamond Chinese <span>103 Carr House Road, Hyde Park,</span>Doncaster DN1 2BD.  </li>
-						<li><a href="tel:+441302368585">+441302368585</a>  </li>
-						<li><a href="mailto:esales@red-diamond-chinese.co.uk">esales@red-diamond-chinese.co.uk</a> </li>
+						<li>Red Diamond Chinese <span>103 Carr House Road, Hyde Park</span>Doncaster DN1 2BD  </li>
+					    <li>+44 1302 368585 </li>
+						<li><a href="mailto:info@example.com">contact@example.com</a> </li>
 					</ul>
 				</div>
 				<div class="clear"></div>
