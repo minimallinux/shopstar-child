7c22ce94ba0715919159a30b0b4fa8b831d26895
minimallinux <anthonymcgowan1@gmail.com>
2017-12-25 23:20:33 +0000

    Update Styles & add opening times/closed signs

--- functions.php
+++ functions.php
@@ -8,12 +8,23 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
 }
-
-/*function shopstar_child_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'shopstar_content_width', 660 );
+// Trigger Holiday Mode
+ add_action ('init', 'bbloomer_woocommerce_holiday_mode');
+// Disable Cart, Checkout, Add Cart
+function bbloomer_woocommerce_holiday_mode() {
+ remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+ remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+ remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+ add_action( 'woocommerce_before_main_content', 'bbloomer_wc_shop_disabled', 5 );
+ add_action( 'woocommerce_before_cart', 'bbloomer_wc_shop_disabled', 5 );
+ add_action( 'woocommerce_before_checkout_form', 'bbloomer_wc_shop_disabled', 5 );
 }
-add_action( 'after_setup_theme', 'shopstar_child_content_width', 0 );*/
-
+// Show Holiday Notice
+function bbloomer_wc_shop_disabled() {
+        wc_print_notice( 'Our Online Shop is Closed Today :)', 'error');
+} 
+/*Set Maximum Variations*/
 define( 'WC_MAX_LINKED_VARIATIONS', 99 );
 /*Proceeed To checkout at Top
 add_action( 'woocommerce_before_cart', 'move_proceed_button' );
